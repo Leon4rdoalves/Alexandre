25da3b4028c830cbbbb520e21a19bbe0659c16f4
Leoanardo Alves <leon4rdoalvess@gmail.com>
2020-07-31 21:01:37 -0300

    Atualizando repositósio

---
+++ curso_completo/nivel1/04_Estrutura de Repetição/01_for.py
@@ -0,0 +1,18 @@
+"""
+ESTRUTURA DE REPETIÇÃO
+
+For >>> Utilizada quando se sabe a quantidade de repetições que serão necessárias, de forma que é obrigatório determinar
+o final da execução do laço.
+
+Sintaxe >>> for item in iteravel:
+                execução
+
+* Range
+    Ex. Contador 10 - 0
+                 0 - 10
+                  step
+
+* Enumerate
+     Ex. for valor in enumerate(frase)
+
+"""
